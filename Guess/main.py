65b2ed399be7712223fbdbfbf552ac4ec5713e35
Frug4l <maximencoda@gmail.com>
2025-12-02 23:17:50 +0300

    меню

--- Guess/main.py
+++ Guess/main.py
@@ -10,3 +10,29 @@ class GuessNumberGame:
         # Переменные игры
         self.secret_number = random.randint(1, 100)
         self.attempts = 0
+        
+        def create_menu(self):
+        """Создание меню приложения - обязательное требование"""
+        menubar = tk.Menu(self.root)
+        self.root.config(menu=menubar)
+        
+        # Меню "Файл"
+        file_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Файл", menu=file_menu)
+        file_menu.add_command(label="Новая игра", command=self.restart_game)
+        file_menu.add_separator()
+        file_menu.add_command(label="Выход", command=self.root.quit)
+        
+        # Меню "Настройки" (обязательное требование - настройка размеров окна)
+        settings_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Настройки", menu=settings_menu)
+        settings_menu.add_command(
+            label="Изменить размер окна", 
+            command=self.resize_window  # обязательная функциональность
+        )
+        
+        # Меню "Справка"
+        help_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Справка", menu=help_menu)
+        help_menu.add_command(label="Правила", command=self.show_rules)
+        help_menu.add_command(label="О программе", command=self.show_about)
